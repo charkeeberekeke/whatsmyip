d7991804970a2f606774f79e58ba1a399428e7b7
root <root@vagrant-ubuntu-precise-32.(none)>
2014-09-04 03:38:11 +0000

    Added GeoIP

--- whatsmyip/views.py
+++ whatsmyip/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import GeoIP
 
 # Create your views here.
 
 def index(request):
     ip = request.META["REMOTE_ADDR"]
-    return HttpResponse("Your IP address is " + ip)
-
+    gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
+    return HttpResponse("Hello " + ip +  " from " + str(gi.country_name_by_addr(ip)))
