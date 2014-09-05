64ddf1a02bd1980f39539ea398180a276959c9c7
root <root@vagrant-ubuntu-precise-32.(none)>
2014-09-05 01:26:03 +0000

    Added reverse dns lookup

--- whatsmyip/views.py
+++ whatsmyip/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import GeoIP
+from dns import resolver,reversename
 
 # Create your views here.
 
 def index(request):
     ip = request.META["REMOTE_ADDR"]
-    gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
-    return HttpResponse("Hello " + ip +  " from " + str(gi.country_name_by_addr(ip)))
+    country = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE).country_name_by_addr(ip)
+    if country:
+	fr = "from " + country
+    else:
+        fr = ""
+    try:
+        addr = reversename.from_address(ip)
+        name = str(resolver.query(addr,"PTR")[0]) + "( " + ip + " ) "
+    except (resolver.NXDOMAIN, resolver.NoAnswer): # no reverse dns lookup match
+        name = ip
+
+    return HttpResponse("Hello " + name + fr)
